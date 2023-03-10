4ace9691187ec54e571189dd6403ecc8f48a8674
Cyan <68551684+Chinese-Cyq20100313@users.noreply.github.com>
2023-03-10 13:08:02 +0800

    test : Fix tests using async

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,31 +8,31 @@ from config import ADMIN_CODE
 
 
 class FileAPITest(unittest.TestCase):
-    def setUp(self) -> None:
+    async def setUp(self) -> None:
         self.app = app.test_client()
-        resp = self.app.get('/')
+        resp = await self.app.get('/')
         self.failUnlessEqual(resp.content_type, 'application/json')
         self.failUnlessEqual(('name', 'versionCode', 'version'), tuple(resp.json.keys()))
 
-    def test_01_1_upload(self):
+    async def test_01_1_upload(self):
         """
         根目录上传
         """
 
         data = {'file': (BytesIO(f"test_01".encode('UTF-8')), 'test_01')}
 
-        resp = self.app.put('/api/file/tests/', data=data)
+        resp = await self.app.put('/api/file/tests/', data=data)
 
         self.assertEqual(resp.content_type, 'application/json', 'Bad Content-Type')
         self.assertEqual(resp.json['status'], 200, resp.json['msg'])
         self.assertEqual(resp.status_code, HTTPStatus.CREATED)
 
-    def test_01_2_get(self):
+    async def test_01_2_get(self):
         """
         根目录获取(单个文件)
         """
 
-        resp = self.app.get('/api/file/tests/test_01')
+        resp = await self.app.get('/api/file/tests/test_01')
 
         if resp.content_type == 'application/json':
             self.assertTrue(False, resp.json)
@@ -40,11 +40,11 @@ class FileAPITest(unittest.TestCase):
 
         self.assertEqual(resp.data, b'test_01')
 
-    def test_01_3_dir1(self):
+    async def test_01_3_dir1(self):
         """
         列出根目录(tests/)
         """
-        response = self.app.get('/api/file/tests/')
+        response = await self.app.get('/api/file/tests/')
 
         d_next = response.json['data']['next']
 
@@ -52,19 +52,19 @@ class FileAPITest(unittest.TestCase):
             self.assertNotEqual(d_next, -114514, 'Something went wrong!')
 
         while d_next > 0:
-            response = self.app.get(f'/api/file/tests/?next={d_next}')
+            response = await self.app.get(f'/api/file/tests/?next={d_next}')
 
             d_next = response.json['data']['next']
 
         self.assertNotEqual(response.json['data']['next'], -114514, 'Something went wrong!')
         self.assertEqual(response.json['data']['next'], 0, "Invalid next value")
 
-    def test_02_01_create_dir(self):
+    async def test_02_01_create_dir(self):
         """
         创建文件夹
         """
 
-        resp = self.app.put('/api/file/tests/test_02/')
+        resp = await self.app.put('/api/file/tests/test_02/')
 
         # resp.json 即将返回数据转换为 json
         # resp.data 即返回数据
@@ -72,24 +72,24 @@ class FileAPITest(unittest.TestCase):
         self.assertEqual(resp.json['status'], 200, resp.json['msg'])
         self.assertEqual(resp.status_code, HTTPStatus.CREATED)
 
-    def test_02_02_upload_to_dir(self):
+    async def test_02_02_upload_to_dir(self):
         """
         向文件夹内上传文件
         """
         data = {'file': (BytesIO(b"test_02_2"), 'test_02_2')}
 
-        resp = self.app.put('/api/file/tests/test_02/', data=data)
+        resp = await self.app.put('/api/file/tests/test_02/', data=data)
 
         self.assertEqual(resp.content_type, 'application/json', 'Bad Content-Type')
         self.assertEqual(resp.json['status'], 200, resp.json['msg'])
         self.assertEqual(resp.status_code, HTTPStatus.CREATED)
 
-    def test_02_03_dir(self):
+    async def test_02_03_dir(self):
         """
         列出文件夹(tests/test_02/)内容
         """
 
-        response = self.app.get('/api/file/tests/test_02/')
+        response = await self.app.get('/api/file/tests/test_02/')
 
         lst: FileMapping = [*response.json['data']['files']]
 
@@ -101,7 +101,7 @@ class FileAPITest(unittest.TestCase):
         while d_next > 0:
             lst.append(*response.json['data']['files'])
 
-            response = self.app.get(f'/api/file/tests/test_02/?next={d_next}')
+            response = await self.app.get(f'/api/file/tests/test_02/?next={d_next}')
 
             d_next = response.json['data']['next']
 
